b44a9fedd29384d95482607af9b3b71d3eac74dc
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-04-16 18:26:44 -0400

    Created the ability to add incomes and expenses as well as the ability to see current account total.

--- javaProject/methods/Transaction.java
+++ javaProject/methods/Transaction.java
@@ -1,12 +1,26 @@
 package javaProject.methods;
 
+import java.util.ArrayList;
+
 public class Transaction {
     private int purchasePrice;
     private int newAccountTotal;
+    private ArrayList<Integer> transactionList;
 
-    public int transaction(int price, int accountTotal){
+    public int transactionBuy(int price, int accountTotal){
         this.purchasePrice = price;
         this.newAccountTotal = accountTotal - price;
+        this.transactionList.add(-price);
         return(-price);
+    }
+    public int transactionEarn(int price, int accountTotal){
+        this.purchasePrice = price;
+        this.newAccountTotal = accountTotal + price;
+        this.transactionList.add(price);
+        return(price);
+    }
+
+    public int getAccountTotal(){
+        return(this.newAccountTotal);
     }
 }
