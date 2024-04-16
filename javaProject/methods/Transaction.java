08e74adf6de3d5006b82937e3f5273997d1a9b75
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-04-16 17:52:33 -0400

    Added transaction function

--- javaProject/methods/Transaction.java
+++ javaProject/methods/Transaction.java
@@ -1,4 +1,12 @@
 package javaProject.methods;
 
 public class Transaction {
+    private int purchasePrice;
+    private int newAccountTotal;
+
+    public int transaction(int price, int accountTotal){
+        this.purchasePrice = price;
+        this.newAccountTotal = accountTotal - price;
+        return(-price);
+    }
 }
