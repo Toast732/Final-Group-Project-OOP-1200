bbe66183eb94e7817a81a9e8ff658c640d1fedfe
Liam Matthews <55813762+Toast732@users.noreply.github.com>
2024-04-19 16:40:21 -0400

    Income/Expenses separated.

--- javaProject/pages/AddIncomePage.java
+++ javaProject/pages/AddIncomePage.java
@@ -4,23 +4,27 @@ import javaProject.accounts.users.UserHandler;
 import javaProject.methods.User;
 import javaProject.pageSegments.PopupSegment;
 import javaProject.panels.DropdownPanel;
+import javaProject.transactions.OneTimeTransaction;
 import javaProject.transactions.RegularTransaction;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class AddRegularTransactionPage extends NormalPage {
+public class AddIncomePage extends NormalPage {
 
-    public AddRegularTransactionPage() {
-        super("Add Regular Transaction", new FlowLayout());
+    public AddIncomePage() {
+        super("Add Income", new FlowLayout());
 
         // Create the dropdown panel
-        DropdownPanel transactionTypeDropdownPanel = new DropdownPanel("Transaction Type:");
+        DropdownPanel transactionTypeDropdownPanel = new DropdownPanel("Income Type:");
 
         // Add the items to the dropdown
         transactionTypeDropdownPanel.addDropdownItem("Yearly", this::showYearlyMenu);
 
         transactionTypeDropdownPanel.addDropdownItem("Hourly", this::showHourlyMenu);
+
+        // Add the one time
+        transactionTypeDropdownPanel.addDropdownItem("One-time", this::showOneTimeMenu);
 
         this.jPanel.add(transactionTypeDropdownPanel);
     }
@@ -33,7 +37,7 @@ public class AddRegularTransactionPage extends NormalPage {
         JPanel transactionGrid = new JPanel(new GridLayout(2, 2));
 
         // Add a label & field for the transaction name.
-        JLabel transactionNameLabel = new JLabel("Transaction Name:");
+        JLabel transactionNameLabel = new JLabel("Income Source Name:");
 
         JPanel transactionNamePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
@@ -50,7 +54,7 @@ public class AddRegularTransactionPage extends NormalPage {
         transactionGrid.add(transactionNameFieldPanel);
 
         // Create the label for the yearly transaction amount
-        JLabel yearlyTransactionAmountLabel = new JLabel("Yearly Transaction Amount:");
+        JLabel yearlyTransactionAmountLabel = new JLabel("Yearly Income:");
 
         // Create the panel for the yearly transaction amount
         JPanel yearlyTransactionAmountPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -124,7 +128,7 @@ public class AddRegularTransactionPage extends NormalPage {
         JPanel transactionGrid = new JPanel(new GridLayout(3, 2));
 
         // Add a label & field for the transaction name.
-        JLabel transactionNameLabel = new JLabel("Transaction Name:");
+        JLabel transactionNameLabel = new JLabel("Income Source:");
 
         JPanel transactionNamePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
@@ -233,5 +237,97 @@ public class AddRegularTransactionPage extends NormalPage {
 
         // Return.
         return hourlyPanel;
+    }
+
+    private JPanel showOneTimeMenu() {
+
+        JPanel oneTimePanel = new JPanel(new GridLayout(2, 1));
+
+        JPanel transactionGrid = new JPanel(new GridLayout(2, 2));
+
+        // Add a label & field for the transaction name.
+        JLabel transactionNameLabel = new JLabel("Income Source Name:");
+
+        JPanel transactionNamePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        transactionNamePanel.add(transactionNameLabel);
+
+        JTextField transactionNameField = new JTextField(32);
+
+        JPanel transactionNameFieldPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        transactionNameFieldPanel.add(transactionNameField);
+
+        transactionGrid.add(transactionNamePanel);
+
+        transactionGrid.add(transactionNameFieldPanel);
+
+        // Create the label for the one time transaction amount
+        JLabel oneTimeTransactionAmountLabel = new JLabel("Amount:");
+
+        // Create the panel for the one time transaction amount
+        JPanel oneTimeTransactionAmountPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        // Add the label to the panel
+        oneTimeTransactionAmountPanel.add(oneTimeTransactionAmountLabel);
+
+        // Create the text field for the one time transaction amount
+        JTextField oneTimeTransactionAmountField = new JTextField(32);
+
+        // Create the panel for the one time transaction amount field
+        JPanel oneTimeTransactionAmountFieldPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        // Add the text field to the panel
+        oneTimeTransactionAmountFieldPanel.add(oneTimeTransactionAmountField);
+
+        // Add the labels to the grid.
+        transactionGrid.add(oneTimeTransactionAmountPanel);
+
+        transactionGrid.add(oneTimeTransactionAmountFieldPanel);
+
+        // Add the grid to the panel.
+        oneTimePanel.add(transactionGrid);
+
+        // Add a submit button to the page to submit the transaction.
+        JButton submitButton = new JButton("Submit");
+
+        // Add the submit button to the page.
+        oneTimePanel.add(submitButton);
+
+        // Call a method whenever the button is clicked.
+        submitButton.addActionListener(e -> {
+
+            // Get the text in the transaction name field.
+            String transactionName = transactionNameField.getText();
+
+            // Get the text in the one time transaction amount field.
+            String oneTimeTransactionAmount = oneTimeTransactionAmountField.getText();
+
+            // Create a try/catch to catch errors from any of the fields.
+            try {
+                // Create a new regular transaction.
+                OneTimeTransaction oneTimeTransaction = new OneTimeTransaction(transactionName);
+
+                // Set the hourly amount.
+                oneTimeTransaction.setAmount(Double.parseDouble(oneTimeTransactionAmount));
+
+                // Get the current user.
+                User user = UserHandler.getInstance().getUser();
+
+                // Add the transaction to the user.
+                user.transactions.add(oneTimeTransaction);
+
+            } catch (NumberFormatException ex) {
+                // Set the segment to the popup segment, tell it to play the sound.
+                new PopupSegment(
+                        "Error",
+                        "Please enter a valid number in each of the fields.",
+                        true
+                );
+            }
+        });
+
+        // Return.
+        return oneTimePanel;
     }
 }
